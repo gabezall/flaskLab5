821a3bbab653b76dc3affbf995091e93350e4acf
gabezall <gzall@ithaca.edu>
2020-12-03 10:57:33 -0500

    still working on lab 6

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
+# temp commit
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
